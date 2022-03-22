96166ffe509f12dbcbbe168170ee686d09b05db3
ajoy267 <andrew.joy12@hotmail.com>
2022-03-22 15:10:06 -0700

    wrote the get all for the reviewers

--- __tests__/reviewers.test.js
+++ __tests__/reviewers.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Reviewer = require('../lib/models/Reviewer');
 
 describe('local-bookstore reviewer routes', () => {
   beforeEach(() => {
@@ -19,5 +20,11 @@ describe('local-bookstore reviewer routes', () => {
     };
     const res = await request(app).post('/api/v1/reviewers').send(expected);
     expect(res.body).toEqual({ id: expect.any(String), ...expected });
+  });
+
+  it('gets all reviewers', async () => {
+    const expected = await Reviewer.getAllReviewers();
+    const res = await request(app).get('/api/v1/reviewers');
+    expect(res.body).toEqual(expected);
   });
 });
