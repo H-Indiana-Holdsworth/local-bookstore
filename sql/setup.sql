a2f2077b5b5376ed49274ff7bd66dfca5dac0c50
ajoy267 <andrew.joy12@hotmail.com>
2022-03-22 11:13:46 -0700

    wrote the first create for publisher

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,16 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS publishers;
+
+CREATE TABLE publishers(
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    name TEXT NOT NULL,
+    city TEXT NOT NULL,
+    state TEXT NOT NULL,
+    country TEXT NOT NULL
+);
+
+INSERT INTO 
+    publishers (name, city, state, country)
+VALUES 
+    ('Pindy', 'Prescott', 'AZ', 'USA');
