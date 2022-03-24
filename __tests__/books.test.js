08f2980a775cdc11c01dc120ec5d9ecbe2204e7c
ajoy267 <andrew.joy12@hotmail.com>
2022-03-24 10:40:04 -0700

    passed test for getAllBooks

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Book = require('../lib/models/Book');
 
 describe('local-bookstore book routes', () => {
   beforeEach(() => {
@@ -22,5 +23,11 @@ describe('local-bookstore book routes', () => {
     const res = await request(app).post('/api/v1/books').send(expected);
 
     expect(res.body).toEqual({ id: expect.any(String), ...expected });
+  });
+
+  it('gets all books', async () => {
+    const expected = await Book.getAllBooks();
+    const res = await request(app).get('/api/v1/books');
+    expect(res.body).toEqual(expected);
   });
 });
