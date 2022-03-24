e81b0654969b076e25aba62d3973c0247370a586
Hayden Indiana Holdsworth <IndianaHoldsworth@gmail.com>
2022-03-24 16:13:50 -0700

    Write half of get 100 reviews fxnality

--- __tests__/reviews.test.js
+++ __tests__/reviews.test.js
@@ -26,6 +26,14 @@ describe('local-bookstore book routes', () => {
     expect(res.body).toEqual({ id: expect.any(String), ...expected });
   });
 
+  it('gets 100 reviews', async () => {
+    const expected = await Review.getListOfReviews();
+
+    const res = await request(app).get('/api/v1/reviews');
+
+    expect(res.body).toEqual(expected);
+  });
+
   it('gets a review by id', async () => {
     const review = await Review.createReview({
       rating: 3,
